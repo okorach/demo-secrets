d88e5838cb21f48cc11c6cf704112acda075d7e5
Olivier Korach <olivier.korach@gmail.com>
2025-07-10 13:12:59 +0200

    Change

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 
 sonar-scanner -Dsonar.token=sqa_91507353f210a6e0ed8eaa338e7473c6e1c95569
 
-companyConnectionToken=CORP_SECRET_6736695212
 
+
+
+companyConnectionToken=CORP_SECRET_6736695212
 
 connect $companyConnectionToken
